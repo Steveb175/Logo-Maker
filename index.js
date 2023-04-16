c50631a7e26e6491287f82362dd2bb70c0908a9a
Stephen Bondurant <stephenbondurant2014@gmail.com>
2023-04-16 09:34:55 -0500

    Text now appears on top of shape

--- index.js
+++ index.js
@@ -81,8 +81,8 @@ const promptUser = () => {
       // Generate logo
       let logo = `<?xml version="1.0" encoding="UTF-8" standalone="no"?>\n`;
       logo += `<svg xmlns="http://www.w3.org/2000/svg" width="300" height="200">\n`;
-      logo += `<text x="120" y="50" fill="${answers.textColor}">${answers.characters}</text>\n`;
       logo += shape.render() + "n";
+      logo += `<text x="140" y="100" font-size="30px" fill="${answers.textColor}">${answers.characters}</text>\n`;
       logo += `</svg>`;
 
       // Writes file to examples folder and names it ${logoName}
@@ -97,5 +97,3 @@ const promptUser = () => {
 
 // Call promptUser
 promptUser();
-
-//regex aqua|aliceblue|antiquewhite|black|blue|blueviolet|brown|burlywood|cadetblue|chartreuse|chocolate|coral|cornflowerblue|cornsilk|crimson|cyan|darkblue|darkcyan|darkgoldenrod|darkgray|darkgreen|darkkhaki|darkmagenta|darkolivegreen|darkorange|darkorchid|darkred|darksalmon|darkseagreen|darkslateblue|darkslategray|darkturquoise|darkviolet|deeppink|deepskyblue|dimgray|dodgerblue|firebrick|floralwhite|forestgreen|fuchsia|gainsboro|ghostwhite|gold|goldenrod|gray|green|greenyellow|honeydew|hotpink|indianred|indigo|ivory|khaki|lavender|lavenderblush|lawngreen|lemonchiffon|lightblue|lightcoral|lightcyan|lightgoldenrodyellow|lightgray|lightgreen|lightpink|lightsalmon|lightseagreen|lightskyblue|lightslategray|lightsteelblue|lightyellow|lime|limegreen|linen|magenta|maroon|mediumaquamarine|mediumblue|mediumorchid|mediumpurple|mediumseagreen|mediumslateblue|mediumspringgreen|mediumturquoise|mediumvioletred|midnightblue|mintcream|mistyrose|moccasin|navajowhite|navy|oldlace|olive|olivedrab|orange|orangered|orchid|palegoldenrod|palegreen|paleturquoise|palevioletred|papayawhip|peachpuff|peru|pink|plum|powderblue|purple|red|rosybrown|royalblue|saddlebrown|salmon|sandybrown|seagreen|seashell|sienna|silver|skyblue|slateblue|slategray|snow|springgreen|steelblue|tan|teal|thistle|tomato|turquoise|violet|wheat|white|whitesmoke|yellow|yellowgreen
